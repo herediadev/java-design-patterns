1dd7b1beaf8972458403f2f4a2d3c31f0883982a
Rafael Heredia Bastidas <heredia.developer@gmail.com>
2020-06-26 01:09:59 -0400

    singleton pattern lazy loaded

--- singlenton/src/main/demo2/DbSingleton.java
+++ singlenton/src/main/demo2/DbSingleton.java
@@ -2,12 +2,15 @@ package main.demo2;
 
 public class DbSingleton {
 
-    private static final DbSingleton instance = new DbSingleton();
+    private static DbSingleton instance;
 
     private DbSingleton() {
     }
 
     public static DbSingleton getInstance() {
+        if (instance == null)
+            instance = new DbSingleton();
+
         return instance;
     }
 }
