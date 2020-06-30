051a3b8103c2657e2be83e70121644c19d867d66
Rafael Heredia Bastidas <heredia.developer@gmail.com>
2020-06-30 00:14:30 -0400

    builder pattern added notes

---
+++ builder/src/main/demo1/BuilderDemo1.java
@@ -0,0 +1,21 @@
+package main.demo1;
+
+public class BuilderDemo1 {
+    public static void main(String[] args) {
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("This is an example");
+
+        builder.append("of the builder pattern. ");
+
+        builder.append("It has methods to append ");
+
+        builder.append("almost anything we want to a String. ");
+
+        builder.append(42);
+
+        System.out.println(builder.toString());
+
+    }
+}
